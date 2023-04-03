4cf18e06d4403e70978bbc2ab6205b8df09ef4e0
김성훈 <pole@1024corp.com>
2023-04-03 11:37:25 +0900

    node mySQL CRUD맛보기

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const mysql = require("mysql");
 const dbConfig = require("./app/config/db.config");
 const app = express();
-
+const ejs = require('ejs');
+const {param} = require("express/lib/router");
 const port = 3000;
 
 
@@ -19,18 +21,56 @@ con.connect(function (err) {
     if(err) throw err;
     console.log('db연결 ')
 });
-const sql = 'INSERT INTO users(name,email) VALUES(?,?)'
-con.query(sql,['Jack','jack@exsample.co.jp'],function (err,result,fields) {
-    if(err )throw err;
-    console.log(result)
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+//조회
+app.get ('/',(req,res) => {
+    const sql  = 'select * from users' ;
+    con.query(sql,function (err,result,fields) {
+        if(err) throw err;
+        res.render('index',{users:result});
+    })
+})
+//딜리트
+app.get('/delete/:id', (req,res) => {
+    const sql ='DELETE FROM users WHERE id = ?';
+    con.query(sql,[req.params.id],function (err,result,fields) {
+        if(err) throw  err;
+        console.log(result);
+        res.redirect('/')
+    })
 })
 
+//수정
 
-app.get('/',(req,res) => {
-    const sql = 'select * from users'
-    con.query(sql,function (err,result,fields) {
+app.get('/edit/:id',(req,res) => {
+    const sql = 'SELECT * FROM users WHERE id = ? ';
+    con.query(sql,[req.params.id],function (err,result,fields) {
+        if(err )throw err;
+        res.render('edit',{user: result});
+    })
+})
+app.post('/update/:id',(req,res) => {
+    const sql = 'UPDATE users SET ? WHERE id = ' + req.params.id;
+    con.query(sql,req.body,function (err,result,fields) {
         if(err) throw  err;
-        res.send(result);
+        console.log(result);
+        res.redirect('/');
+    })
+})
+
+app.get('/create',(req,res) =>
+    res.sendFile(path.join(__dirname,'form.html')))
+
+app.post('/',(req,res) => {
+    const sql  ='INSERT INTO users SET ?';
+
+    con.query(sql,req.body,function (err,result,fields) {
+        if(err) throw err;
+        console.log(result);
+        res.send('등록이 완료되었습니다.')
     })
 })
 
